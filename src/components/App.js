bc16ee767fe050e10dac1447b1be0e29c4d2ca92
Paolo <paoloagva@gmail.com>
2022-02-10 23:26:48 +0100

    Added navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-    const route = window.location.pathname;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage></HomePage>;
+    function getPage() {
+        const route = window.location.pathname;
+        if (route === "/about") return <AboutPage />;
+        return <HomePage></HomePage>;
+    }
+
+    return (
+        <div className="container-fluid">
+            <Header></Header>
+            {getPage()}
+        </div>
+    );
 }
 
 export default App;
